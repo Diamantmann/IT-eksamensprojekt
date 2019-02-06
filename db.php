21e7d89c80d78ac39323c4b5ec262518131c805e
Christian6000 <46445109+Christian6000@users.noreply.github.com>
2019-02-06 15:11:30 +0100

    Add files via upload

--- db.php
+++ db.php
@@ -3,7 +3,7 @@ include 'funktionsark.php';
 
 $servername = "localhost";
 $username = "root";
-$password = "";
+$password = "neger";
 $conn = new mysqli($servername, $username, $password);
 
 if ($conn->connect_error) {
@@ -15,9 +15,9 @@ if ($conn->connect_error) {
 $sql = "CREATE DATABASE IF NOT EXISTS investeringstips";
 
 if ($conn->query($sql) === TRUE) {
-	echo "Database 'investeringstips' er oprettet";
+	echo "Database 'investeringstips' er oprettet"."<br>";
 } else {
-	echo "Fejl ved oprettelse af databasen: " . $conn->error;
+	echo "Fejl ved oprettelse af databasen: " . $conn->error."<br>";
 }
 
 $sql = "CREATE TABLE IF NOT EXISTS investeringstips.bruger (
@@ -28,15 +28,17 @@ $sql = "CREATE TABLE IF NOT EXISTS investeringstips.bruger (
 )";
 
 if ($conn->query($sql) === TRUE) {
-	echo "Tabellen 'bruger' er oprettet";
+	echo "Tabellen 'bruger' er oprettet"."<br>";
 } else {
-	echo "Fejl ved oprettelse af tabellen 'bruger'";
+	echo "Fejl ved oprettelse af tabellen 'bruger'"."<br>";
 }
 
 $sql = "CREATE TABLE IF NOT EXISTS investeringstips.indlaeg (
 	indlaegsID INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	titel VARCHAR(100),
 	indhold VARCHAR(300),
-	brugerIDForIndlaegger INT(6),
+	brugerIDForIndlaegger INT(6) UNSIGNED,
+	FOREIGN KEY (brugerIDForIndlaegger) REFERENCES bruger(brugerID),
 	datoForIndlaeg DATETIME,
 	upvotes INT(6),
 	downvotes INT(6),
@@ -44,12 +46,20 @@ $sql = "CREATE TABLE IF NOT EXISTS investeringstips.indlaeg (
 )";
 
 if ($conn->query($sql) === TRUE) {
-	echo "Tabellen 'indlæg' er oprettet";
+	echo "Tabellen 'indlæg' er oprettet"."<br>";
 } else {
-	echo "Fejl ved oprettelse af tabellen 'indlæg'";
+	echo "Fejl ved oprettelse af tabellen 'indlæg'"."<br>";
 }
 
-if (!empty($_GET["Titel"]) && !empty($_GET["Indhold"])) {
-	//$conn->query
+if (!empty($_GET["titel"]) && !empty($_GET["indhold"])) {
+	$sql = "INSERT INTO investeringstips.indlaeg (
+	titel, indhold, brugerIDForIndlaegger, datoForIndlaeg, upvotes, downvotes, guld) 
+	VALUES ('".$_GET["titel"]."', '".$_GET["indhold"]."', 2, '2019-02-02 1:11:11', 0, 0, 0)";
+}
+
+if ($conn->query($sql) === TRUE) {
+	echo "Indlæg indsendt"."<br>";
+} else {
+	echo "Fejl ved indsendelse af indlæg"."<br>";
 }
 ?>
--- forside.php
+++ forside.php
@@ -4,9 +4,22 @@
     </head>
     <body>
         <form action="db.php" method="get">
-            Titel:       <input type="text" name="Titel"><br>
-            Indhold:   <input type="text" name="Indhold"><br>
+            Titel:       <input type="text" name="titel"><br>
+            Indhold:   <input type="text" name="indhold"><br>
             <input type="submit" value="Indsend indlaeg">
         </form>
     </body>
 </html>
+
+<?php
+
+$sql = "SELECT titel, indhold, guld, datoForIndlaeg FROM investeringstips.indlaeg WHERE brugerIDForIndlaegger = 2";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        echo "<br>" . $row["titel"]. "<br>" . $row["indhold"] . "<br>Guld: " . $row["guld"] . "<br>Dato for indlæg: " . $row["datoForIndlaeg"];
+    }
+}
+
+?>
