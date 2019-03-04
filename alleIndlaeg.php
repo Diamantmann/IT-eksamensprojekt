c9e15686166be0002bb59c31eb5d42284d0b14a0
Christian6000 <46445109+Christian6000@users.noreply.github.com>
2019-03-04 10:16:53 +0100

    Add files via upload

--- alleIndlaeg.php
+++ alleIndlaeg.php
@@ -2,22 +2,23 @@
 
 include 'forbindelse.php';
 
-$sql = "SELECT titel, indhold, guld, datoForIndlaeg FROM investeringstips.indlaeg ORDER BY guld DESC";
+$sql = "SELECT indlaegsID, titel, indhold, guld, datoForIndlaeg FROM investeringstips.indlaeg ORDER BY guld DESC";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
-    	$i = $i + 1;
-        echo "<br>Titel: " . $row["titel"]. "<br>Indlæg: " . $row["indhold"] . "<br>Guld: " . $row["guld"] . "<br>Dato for indlæg: " . $row["datoForIndlaeg"] . "<br>
+        echo "<br>Titel: " . $row["titel"]. "<br>Indlæg: " . $row["indhold"] . "<br>Guld: " . $row["guld"] . "<br>Dato for indlæg: " . $row["datoForIndlaeg"] . "<br>";
+        ?>
 
-        <form action='guld.php' method='get'>
-            <input type='submit' value='Upvote'>
+        <form action="guld.php" method="get">
+            <input type="hidden" name="upvote" value="<?php echo $row["indlaegsID"]?>">
+            <input type="submit" value="Upvote">
         </form>
 
-        <form action='guld.php?downvote=TRUE&indlaegsID=1' method='get'>
-            <input type='submit' value='Downvote'>
-        </form>";
+        <form action="guld.php" method="get">
+            <input type="submit' value="Downvote">
+        </form>
+        <?php
     }
 }
-
 ?>
