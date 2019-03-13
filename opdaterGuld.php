1d5ef69334235749f4fd1433cb4aa9e0175c60a2
Diamantmann <46533897+Diamantmann@users.noreply.github.com>
2019-03-13 15:23:11 +0100

    Fuck php
    LOOOL

--- opdaterGuld.php
+++ opdaterGuld.php
@@ -1,13 +1,19 @@
 <?php
 
-session_start();
 
 $IID = $_SESSION['IID'];
 
-$sql = "SELECT upvoteID, upvoteEllerDownvote as f FROM investeringstips.upvotetransaktioner i1
+$sql = "SELECT upvoteEllerDownvote as guldsum, indlaegsIDForIndlaeg FROM investeringstips.upvotetransaktioner i1
 inner join
 (
-SELECT MAX(upvoteID) as maksi, upvoteEllerDownvote as g FROM investeringstips.upvotetransaktioner GROUP BY brugerIDForUpvoter
+SELECT MAX(upvoteID) as maksi, upvoteEllerDownvote as g FROM investeringstips.upvotetransaktioner WHERE indlaegsIDForIndlaeg=1 GROUP BY brugerIDForUpvoter
     ) i2 on i1.upvoteID = i2.maksi)";
 
+$guldSum = $conn->query($sql);
+if ($guldSum->num_rows > 0) {
+    while($maengdeGuld = $guldSum->fetch_assoc()) {
+        $guldSummen = $maengdeGuld['guldsum'];
+        $sql = "UPDATE investeringstip.indlaeg SET guld=$guldSummen WHERE indlaegsID=$IID";
+    }
+}
 ?>
