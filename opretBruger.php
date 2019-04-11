e2f753caaeb052c6dcb549f8d0d4bfa81a1e2dd4
Neismo <hiffidrengen@gmail.com>
2019-04-11 16:03:10 +0200

    Yep

--- opretBruger.php
+++ opretBruger.php
@@ -5,8 +5,8 @@
     </head>
     <body>
         <form action="opretBrugerFunc.php" method="get">
-            Brugernavn: <input type="text" name="brugernavn"><br>
-            Kodeord:   <input type="password" name="kodeord">
+            <label>Brugernavn:</label> <input type="text" name="brugernavn"><br>
+            <label>Kodeord:</label>   <input type="password" name="kodeord">
             <input type="submit" value="Opret Bruger">
         </form>
     </body>
