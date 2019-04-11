e0de49c2358472ec6c8e17194b4be001fe1d247e
Neismo <hiffidrengen@gmail.com>
2019-04-11 17:02:15 +0200

    bm

--- opretBrugerFunc.php
+++ opretBrugerFunc.php
@@ -6,17 +6,16 @@ $navn = $_GET['brugernavn'];
 $kodeord = $_GET['kodeord'];
 
 //Denne funktion sætter brugeren i systemet, hvis hverken brugernavnet eller kodeordet er tomt.
-if($navn == "" || $kodeord == ""){
+if(empty($navn) || empty($kodeord)){
     echo "Brugernavnet eller kodeordet kan IKKE være tomt";
     include 'opretBruger.php';
+} else if(tjekBrugernavnEksistere($conn,$navn)){
+    lavBruger($conn,$navn,$kodeord);
+    echo "Bruger oprettet - du kan nu logge ind!";
+    include 'index.php';
 } else{
-    if(tjekBrugernavnEksistere($conn,$navn)){
-        lavBruger($conn,$navn,$kodeord);
-        echo "Bruger oprettet - du kan nu logge ind!";
-        include 'index.php';
-    } else{
-        echo "Brugernavn eksistere allerede: overvej et andet brugernavn!";
-        include 'opretBruger.php';   
-    }
+    echo "Brugernavn eksistere allerede: overvej et andet brugernavn!";
+    include 'opretBruger.php';   
 }
+
 ?>
